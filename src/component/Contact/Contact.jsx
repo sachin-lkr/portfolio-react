563a3300ec88fd3a139e3fff7e74b4116d5daa05
sachin <sachin9973083181@gmail.com>
2026-02-21 16:09:52 +0530

    first commit

---
+++ src/component/Contact/Contact.jsx
@@ -0,0 +1,139 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
+const Contact = () => {
+    useEffect(() => {
+          AOS.init({
+            duration: 800,
+            delay: 200,
+            once: false,
+          });
+        }, []);
+
+  const form = useRef();
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setIsSending(true);
+    emailjs
+      .sendForm(
+        "service_oxmj7du",
+        "template_e1ywkxt",
+        form.current,
+        "W70FjRWDtQE-qq6HE"
+      )
+      .then(
+        () => {
+          setIsSending(false);
+          setIsSent(true);
+          form.current.reset();
+          toast.success("Message sent successfully! ✅", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "dark",
+          });
+        },
+        (error) => {
+          console.error("Error sending message:", error);
+          toast.error("Failed to send message. Please try again.", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "dark",
+          });
+        }
+      );
+  };
+
+  return (
+    <section
+      id="contact"
+      className="flex flex-col items-center justify-center py-24 px-[12vw] md:px-[7vw] lg:px-[10vw]"
+    >
+      {/* Toast Container */}
+      <ToastContainer />
+
+      {/* Section Title */}
+      <div className="text-center mb-16">
+        <h2 data-aos="zoom-in" className="text-4xl font-bold text-white">CONTACT</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          I’d love to hear from you—reach out for any opportunities or
+          questions!
+        </p>
+      </div>
+
+      {/* Contact Form */}
+      <div data-aos="zoom-in-up" className="mt-8 w-full max-w-md bg-[#0d081f] p-6 rounded-lg shadow-lg border border-gray-700">
+        <h3 className="text-xl font-semibold text-white text-center">
+          Connect With Me <span className="ml-1">🚀</span>
+        </h3>
+
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="mt-4 flex flex-col space-y-4"
+        >
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Your Email"
+            required
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
+          />
+          <input
+            type="text"
+            name="user_name"
+            placeholder="Your Name"
+            required
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
+          />
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            required
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            rows="4"
+            required
+            className="w-full p-3 rounded-md bg-[#131025] text-white border border-gray-600 focus:outline-none focus:border-purple-500"
+          />
+
+          {/* Send Button */}
+          <button
+            type="submit"
+            disabled={isSending}
+            className={`w-full py-3 text-white font-semibold rounded-md transition
+           ${
+              isSending
+               ? "bg-gray-600 cursor-not-allowed"
+               : "bg-gradient-to-r from-purple-600 to-pink-500 hover:opacity-90"
+            }`}
+          >
+            {isSending ? "Message Sending..." : "Send"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/constants.jsx
@@ -0,0 +1,120 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+
+import cLogo from "./assets/tech_logo/c.png";
+
+import javaLogo from "./assets/tech_logo/java.png";
+
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import bootstrapLogo from "./assets/tech_logo/Bootstrap.png";
+
+import csharpLogo from "./assets/tech_logo/csharp.png";
+
+// Education Section Logo's
+import glaLogo from "./assets/education_logo/munger.png";
+import bsaLogo from "./assets/education_logo/bseb.jpg";
+import vpsLogo from "./assets/education_logo/bseb.jpg";
+
+// Project Section Logo's
+import portfolioPage from "./assets/work_logo/my2ndPortfolio.png";
+import githubSearch from "./assets/work_logo/githubSearch.jpg"
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      { name: "Material UI", logo: materialuiLogo },
+      { name: "Bootstrap", logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: cLogo },
+      { name: "Java", logo: javaLogo },
+      { name: "C-Sharp", logo: csharpLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "TypeScript", logo: typescriptLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+      { name: "Figma", logo: figmaLogo },
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: glaLogo,
+    school: "Internation college Ghosait, Lakhisari (Munger university)",
+    date: "2023 - 2026",
+    desc: "Prsuing BCA with strong interest in Frontend Development.Worked on multiple web projects using React, Tailwind CSS and JavaScript.Developed problem-solving and practical coding skills.",
+    degree: "Bachelor of Computer Applications-BCA",
+  },
+  {
+    id: 1,
+    img: bsaLogo,
+    school: "BSEB(S.R.S +2 H/S Persaban, Lakhisarai)",
+    date: "2017 - 2019",
+    grade: "67.8%",
+    desc: "Completed Higher Secondary Education in Science (PCM) with a strong foundation in Mathematics, Physics, and logical problem-solving. Developed analytical thinking and interest in technology during this period.",
+    degree: "Higher Secondary Education (PCM - Science)",
+  },
+  {
+    id: 2,
+    img: vpsLogo,
+    school: "BSEB(U.M.S. Surari (Lakhisarai))",
+    date: "2016 - 2017",
+    grade: "60%",
+    desc: "Completed secondary school education.",
+    degree: "Secondary School (10th)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Personal Portfolio (Vanilla JS)",
+    description: "Built a fully responsive without any framework",
+    image: portfolioPage,
+    tags: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/sachin-lkr/sachin-lkr.github.io",
+    webapp: "https://sachin-lkr.github.io/",
+  },
+  {
+    id: 1,
+    title: "GitHub Profile Search App",
+    description:
+      "A responsive web application that fetches GitHub user data using GitHub API. Displays profile details like avatar, repositories, followers, and following with real-time search functionality.",
+    image: githubSearch,
+    tags: ["HTML", "CSS", "JavaScript", "GitHub API"],
+    github: "https://github.com/sachin-lkr/github-profile-search",
+    webapp: "https://sachin-lkr.github.io/github-profile-search/",
+  },
+];
